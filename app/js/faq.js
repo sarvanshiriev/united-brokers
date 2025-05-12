fc6dd4746a4a6e57099303574bcd9053921a0102
Sarvan <sarvan.shiriev@mail.ru>
2025-05-12 17:23:22 +0500

    Внесены изменения в проект

--- app/js/faq.js
+++ app/js/faq.js
@@ -1,24 +1,22 @@
 // Логика открытия-закрытия ответов на вопросы
-const faqItems = document.querySelectorAll('.faq__item');
+const faqItems = document.querySelectorAll(".faq__item");
 
-faqItems.forEach(item => {
-    const answer = item.querySelector('.faq__item-answer');
-    const link = item.querySelector('.faq__item-link');
-    item.addEventListener('click', () => {
-        const isAnswerVisible = getComputedStyle(answer).display !== 'none';
+faqItems.forEach((item) => {
+  const answer = item.querySelector(".faq__item-answer");
+  const link = item.querySelector(".faq__item-link");
+  item.addEventListener("click", () => {
+    const isAnswerVisible = getComputedStyle(answer).display !== "none";
 
-        if (!isAnswerVisible) {
-            answer.style.display = 'block';
-            link.classList.toggle('rotate');
-        } else {
-            answer.style.display = 'none';
-            link.classList.toggle('rotate');
-        }
-    });
+    if (!isAnswerVisible) {
+      answer.style.display = "block";
+      link.classList.toggle("rotate");
+    } else {
+      answer.style.display = "none";
+      link.classList.toggle("rotate");
+    }
+  });
 
-    link.addEventListener('click', event => {
-        event.preventDefault();
-    });
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
 });
-
-
--- app/js/footer.js
+++ app/js/footer.js
@@ -1,30 +1,31 @@
 function initMobileFooterToggle() {
-    if (window.innerWidth < 768) {
-      document.querySelectorAll('.footer__list-title').forEach(title => {
-        title.addEventListener('click', () => {
-          const block = title.nextElementSibling;
-  
-          document.querySelectorAll('.footer__list-block').forEach(otherBlock => {
+  if (window.innerWidth < 768) {
+    document.querySelectorAll(".footer__list-title").forEach((title) => {
+      title.addEventListener("click", () => {
+        const block = title.nextElementSibling;
+
+        document
+          .querySelectorAll(".footer__list-block")
+          .forEach((otherBlock) => {
             if (otherBlock !== block) {
-              otherBlock.style.display = 'none';
+              otherBlock.style.display = "none";
             }
           });
-  
-          if (block.style.display === 'block') {
-            block.style.display = 'none';
-          } else {
-            block.style.display = 'block';
-          }
-        });
+
+        if (block.style.display === "block") {
+          block.style.display = "none";
+        } else {
+          block.style.display = "block";
+        }
       });
-    }
+    });
   }
-  
-  document.addEventListener('DOMContentLoaded', initMobileFooterToggle);
-  
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 768) {
-      initMobileFooterToggle();
-    }
-  });
-  
+}
+
+document.addEventListener("DOMContentLoaded", initMobileFooterToggle);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 768) {
+    initMobileFooterToggle();
+  }
+});
